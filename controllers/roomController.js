739ade2dcd115dc8bc8adb300e95fbe140f78705
kholod <kholodkhatib@gmail.com>
2016-08-27 18:41:56 +0300

    room ordering

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -64,15 +64,29 @@ var roomController = function (Room) {
 
 
     var get = function (req, res) {
+
+        /* CheckAndEditDates();*/
         var query = {};
 
-        Room.find(query).sort({'_id': 'descending'}).exec(query, function (err, rooms) {
+        Room.find(query).sort({'_id': 'descending'}).exec(query, function (err, roomsArray) {
             if (err) {
                 console.log(err);
                 res.status(500).send(err);
             } else {
-                res.status(200).send(rooms);
-                // edit and check dates method
+
+                if(roomsArray){
+                    var lastDate=roomsArray[0].lastDate;
+                    var dateNow = new Date();
+                    var timeDiff = Math.abs(dateNow.getTime() - lastDate.getTime());
+                    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+                    if(diffDays!=1){
+                        CheckAndEditDates(roomsArray,diffDays);
+                        res.status(200).send(roomsArray);
+                    }
+                    else{
+                        res.status(200).send(roomsArray);
+                    }
+                }
             }
         });
 
@@ -87,19 +101,23 @@ var roomController = function (Room) {
     * */
     var SaveOrder = function (req, res) {
         //call check date and update
-       /* CheckAndEditDates();*/
+
         var query = {};
         var orderRoom = req.body;
 
         var room=orderRoom.room;
         var date=orderRoom.date;
         var hour=orderRoom.hour;
-        var idUser = orderRoom.userID;
+        var status=orderRoom.status;
+        var idUser =orderRoom.userID;
+
+
         var roomFound={};
 
 
         if (room) {
-            query.name = req.body.room;
+            query._id = req.body._id;
+          /*  query.name = req.body.room;*/
         }
         else
         {
@@ -115,247 +133,54 @@ var roomController = function (Room) {
                 if (roomFound) {
 
                 if (date == "today") {
-                    roomFound.Today.hours[hour - 8]={"hour": hour, "status": false, "user": idUser};
-                    res.status(201).send("order saved");
+                    roomFound.Today.hours[hour - 8]={"hour": hour, "status": status, "user": idUser};
+                    roomFound.update(roomFound,function (e) {
+                        if (e) {
+                            console.log('error: ' + e);
+                            res.status(500).send(err);
+                        } else {
+                            res.status(201).send("order saved");
+
+                        }
+                    });
+
                 }
                 else if (date == "tomorrow") {
 
+                    roomFound.Tomorrow.hours[hour - 8]={"hour": hour, "status": status, "user": idUser};
+                    roomFound.update(roomFound,function (e) {
+                        if (e) {
+                            console.log('error: ' + e);
+                            res.status(500).send(err);
+                        } else {
+                            res.status(201).send("order saved");
 
-                    roomFound.Tomorrow.hours[hour - 8] = ({"hour": hour, "status": false, "user": idUser});
-
-
-                    res.status(201).send("order saved");
+                        }
+                    });
                 }
                 else {
-                    roomFound.Tomorrow.hours[hour - 8] = ({"hour": hour, "status": false, "user": idUser});
+                    roomFound.AfterTomorrow.hours[hour - 8]={"hour": hour, "status": status, "user": idUser};
+                    roomFound.update(roomFound,function (e) {
+                        if (e) {
+                            console.log('error: ' + e);
+                            res.status(500).send(err);
+                        } else {
+                            res.status(201).send("order saved");
 
-                    res.status(201).send("order saved");
+                        }
+                    });
                 }
-                    roomFound.save();
             }
             else{
-                    res.status(500).send(err);
+                    res.status(500).send("room not found");
 
             }
         }});
-       /* Room.findOne(query).sort({'_id': 'descending'}).exec(query, function (err, rooms) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            } else {
-                roomFound=rooms;
-                // edit and check dates method
-            }
-        });*/
-       /* Room.find({}).sort({'_id': 'descending'}).exec(query, function (err, rooms) {
 
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            } else {
-                roomFound=rooms;
-            }
-        });*/
-       /* if(date=="today"){
-            roomFound.Today.hours[8-hour]=({"hour":hour,"status":false,"user":idUser});
-            res.status(201).send("order saved");
-        }
-        else   if(date=="tomorrow"){
-            roomFound.Tomorrow.hours[8-hour]=({"hour":hour,"status":false,"user":idUser});
-            res.status(201).send("order saved");
-        }
-        else{
-            roomFound.Tomorrow.hours[8-hour]=({"hour":hour,"status":false,"user":idUser});
-            res.status(201).send("order saved");
-        }
-*/
-
-
-
-
-        /* if(!roomFound.dates[date]){
-             roomFound.dates.push(date);
-             roomFound.dates[date]=[
-                 {"hour":8,"status":true,"user":idUser},
-                 {"hour":9,"status":true,"user":idUser},
-                 {"hour":10,"status":true,"user":idUser},
-                 {"hour":11,"status":true,"user":idUser},
-                 {"hour":12,"status":true,"user":idUser},
-                 {"hour":13,"status":true,"user":idUser},
-                 {"hour":14,"status":true,"user":idUser},
-                 {"hour":15,"status":true,"user":idUser},
-                 {"hour":16,"status":true,"user":idUser},
-                 {"hour":17,"status":true,"user":idUser},
-             ];
-
-
-             roomFound.dates[date][hour-8]=  {"hour":hour,"status":false,"user":idUser};
-             res.status(201).send("order saved");
-
-
-         }
-         else{
-
-             if( roomFound.dates[date][hour-8].status=true){
-                 roomFound.dates[date][hour-8]=  {"hour":hour,"status":false,"user":idUser};
-                 res.status(201).send("order saved");
-
-             }
-             else{
-                 res.status(500).send("Room is not available");
-             }
-
-         }
- */
-        // cupdate hour
 
 
 
     }
-   /* var SaveOrder = function (req, res) {
-        var orderRoom = req.body;
-        var query = {};
-        var room=orderRoom.room;
-        var date=orderRoom.date;
-        var hour=orderRoom.hour;
-        var idUser = orderRoom.userID;
-        var roomFound={};
-
-
-        if (room) {
-            query.name = new RegExp(room, "i");
-        }
-
-        Room.findOne(query).exec(query, function (err, rooms) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            } else {
-                roomFound=rooms;
-            }
-        });
-
-        query.dates.name = new RegExp("today", "i");
-
-        Room.findOne(query).exec(query, function (err, rooms) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            } else {
-                roomFound=rooms;
-            }
-        });
-
-        if(!room.dates.name){
-            room.dates.push(date);
-            room.dates[date]=[
-                {"hour":8,"status":true,"user":idUser},
-                {"hour":9,"status":true,"user":idUser},
-                {"hour":10,"status":true,"user":idUser},
-                {"hour":11,"status":true,"user":idUser},
-                {"hour":12,"status":true,"user":idUser},
-                {"hour":13,"status":true,"user":idUser},
-                {"hour":14,"status":true,"user":idUser},
-                {"hour":15,"status":true,"user":idUser},
-                {"hour":16,"status":true,"user":idUser},
-                {"hour":17,"status":true,"user":idUser},
-            ];
-
-
-            room.dates[date][hour-8]=  {"hour":hour,"status":false,"user":idUser};
-            res.status(201).send("order saved");
-
-
-        }
-        else{
-
-            if( room.dates[date][hour-8].status=true){
-                room.dates[date][hour-8]=  {"hour":hour,"status":false,"user":idUser};
-                res.status(201).send("order saved");
-
-            }
-            else{
-                res.status(500).send("Room is not available");
-            }
-
-        }
-
-        // cupdate hour
-
-
-
-    }*/
-
-  /*  var SaveOrder = function (req, res) {
-        var orderRoom = req.body;
-        var query = {};
-        var room=orderRoom.room;
-        var date=orderRoom.date;
-        var hour=orderRoom.hour;
-        var idUser = orderRoom.userID;
-        var roomFound={};
-
-
-        if (room) {
-            query.name = new RegExp(room, "i");
-        }
-
-        Room.findOne(query).exec(query, function (err, rooms) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            } else {
-                roomFound=rooms;
-            }
-        });
-console.log(roomFound);
-        if(!roomFound.dates[date]){
-            roomFound.dates.push(date);
-            roomFound.dates[date]=[
-                {"hour":8,"status":true,"user":idUser},
-                {"hour":9,"status":true,"user":idUser},
-                {"hour":10,"status":true,"user":idUser},
-                {"hour":11,"status":true,"user":idUser},
-                {"hour":12,"status":true,"user":idUser},
-                {"hour":13,"status":true,"user":idUser},
-                {"hour":14,"status":true,"user":idUser},
-                {"hour":15,"status":true,"user":idUser},
-                {"hour":16,"status":true,"user":idUser},
-                {"hour":17,"status":true,"user":idUser},
-            ];
-
-
-            roomFound.dates[date][hour-8]=  {"hour":hour,"status":false,"user":idUser};
-            res.status(201).send("order saved");
-
-
-        }
-        else{
-
-            if( roomFound.dates[date][hour-8].status=true){
-                roomFound.dates[date][hour-8]=  {"hour":hour,"status":false,"user":idUser};
-                res.status(201).send("order saved");
-
-            }
-            else{
-                res.status(500).send("Room is not available");
-            }
-
-        }
-
-        // cupdate hour
-
-
-
-    }
-*/
-    /*
-
-     if(!room.dates["15-07-2015"])
-     rooom.dates["15-07-2015"]=[{"09":true},{"09":true}];
-     else
-     */
-
 
     var deleteIt = function (req, res) {
 
@@ -422,7 +247,7 @@ var  DefineRoomDates = function(newRoom){
     return new Room(room);
 }
 
-
+var newlastDate= Date.now();
 
 /*
  * case1: the same date - diff=0
@@ -431,57 +256,101 @@ var  DefineRoomDates = function(newRoom){
  * case 4: diff>2
  * */
 
-    var CheckAndEditDates= function(){
+    var CheckAndEditDates= function(roomsArray,diffDays){
 
 //call query to return rrom
-        for(room in Room){
+var j=0;
+        for( j=0; j< roomsArray.length;j++){
             //case 1
-            if(room.lastDate!=Date.now()){
+
 
                 //get deff between dates
 
-               /* room.Today.date.setDate(room.lastDate.getDate()+1);
-                room.Tomorrow.date.setDate(room.lastDate.getDate()+2);
-                room.AfterTomorrow.date.setDate(room.lastDate.getDate()+3);*/
                 //case 2
-                if(room.lastDate.getDate()+1==Date.now.getDate()){
+                if(diffDays==2){
+
+                    var TomorrowHelp=  roomsArray[j].Tomorrow.hours;
+                    var AfterTomorrowHelp=  roomsArray[j].AfterTomorrow.hours;
+                    roomsArray[j].Today.hours=[];
+                    roomsArray[j].Tomorrow.hours=[];
+                    roomsArray[j].AfterTomorrow.hours=[];
+
                     for (i = 8; i <= 17; i++) {
-                        room.Today.hours=[];
-                        room.Today.hours.push(room.Tomorrow.hours[i-8]);
-                        room.Tomorrow.hours=[];
-                        room.Tomorrow.hours.push(room.AfterTomorrow.hours[i-8]);
-                        room.AfterTomorrow.hours=[];
-                        room.AfterTomorrow.hours.push({hour:i,status:true,user:''});
+
+                        roomsArray[j].Today.hours.push(TomorrowHelp[i-8]);
+                        roomsArray[j].Tomorrow.hours.push(AfterTomorrowHelp[i-8]);
+                        roomsArray[j].AfterTomorrow.hours.push({hour:i,status:true,user:''});
 
                     }
 
 
                 }
-                else if(room.lastDate.getDate()+2==Date.now.getDate()){
+                else if(diffDays==3){
+                    var TomorrowHelp=  roomsArray[j].Tomorrow.hours;
+                   /* var AfterTomorrowHelp=  roomsArray[j].AfterTomorrow.hours;*/
+                    roomsArray[j].Today.hours=[];
+                    roomsArray[j].Tomorrow.hours=[];
+                    roomsArray[j].AfterTomorrow.hours=[];
                     for (i = 8; i <= 17; i++) {
-                        room.Today.hours=[];
-                        room.Today.hours.push(room.Tomorrow.hours[i-8]);
-                        room.Tomorrow.hours=[];
-                        room.Tomorrow.hours.push({hour:i,status:true,user:''});
-                        room.AfterTomorrow.hours=[];
-                        room.AfterTomorrow.hours.push({hour:i,status:true,user:''});
+
+                        roomsArray[j].Today.hours.push(TomorrowHelp[i-8]);
+                        roomsArray[j].Tomorrow.hours.push({hour:i,status:true,user:''});
+                        roomsArray[j].AfterTomorrow.hours.push({hour:i,status:true,user:''});
 
                     }
                 }
                 else{
-                    room.Today.hours=[];
-                    room.Today.hours.push({hour:i,status:true,user:''});
-                    room.Tomorrow.hours=[];
-                    room.Tomorrow.hours.push({hour:i,status:true,user:''});
-                    room.AfterTomorrow.hours=[];
-                    room.AfterTomorrow.hours.push({hour:i,status:true,user:''});
+                    roomsArray[j].Today.hours = [];
+                    roomsArray[j].Tomorrow.hours = [];
+                    roomsArray[j].AfterTomorrow.hours = [];
+                     for (i = 8; i <= 17; i++) {
+
+                         roomsArray[j].Today.hours.push({hour: i, status: true, user: ''});
+                         roomsArray[j].Tomorrow.hours.push({hour: i, status: true, user: ''});
+                         roomsArray[j].AfterTomorrow.hours.push({hour: i, status: true, user: ''});
+
+
+                     }
+
+                   /* for (i = 8; i <= 17; i++) {
+                        room.Today.hours = [];
+                        room.Today.hours.push({hour: i, status: true, user: ''});
+                        room.Tomorrow.hours = [];
+                        room.Tomorrow.hours.push({hour: i, status: true, user: ''});
+                        room.AfterTomorrow.hours = [];
+                        room.AfterTomorrow.hours.push({hour: i, status: true, user: ''});
+
+
+                }*/
+                    roomsArray[j].lastDate=newlastDate;
+                    roomsArray[j].lastDate.setHours(0,0,0,0);
+                    roomsArray[j].update(roomsArray[j],function (e) {
+                        if (e) {
+                            console.log('error: ' + e);
+                            res.status(500).send(err);
+                        } else {
+                            console.log('no error');
+
+                        }
+                    });
+                }
+            //save room
+            roomsArray[j].lastDate=newlastDate;
+            roomsArray[j].lastDate.setHours(0,0,0,0);
+            roomsArray[j].update(roomsArray[j],function (e) {
+                if (e) {
+                    console.log('error: ' + e);
+                    res.status(500).send(err);
+                } else {
+                    console.log('no error');
 
                 }
-                room.lastDate.setDate(Date.now.getDate());
+            });
+
+
             }
 
-        }
-        return true;
+
     }
 
     return {
